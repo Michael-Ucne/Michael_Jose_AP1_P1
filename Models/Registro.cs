13c24174a5afbdec47ac36397397af7152e84be6
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-09 19:16:54 -0400

    Agrega Fecha al Models de Registro

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -7,10 +7,13 @@ namespace Michael_Jose_AP1_P1.Models
         [Key]
         public int AporteId { get; set; }
 
+        public DateTime Fecha { get; set; }
+
         [Required(ErrorMessage = "El Nombre es requerido")]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "El Monto de Aporte es requerido")]
         public double Mount{ get; set; }
+
     }
 }
