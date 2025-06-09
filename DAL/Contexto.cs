be806adba0505f74546573cc8f2549e760147fa6
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-09 18:37:34 -0400

    Agrega el contexto de Base de Datos

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,6 +1,10 @@
-﻿namespace Michael_Jose_AP1_P1.DAL
+﻿using Michael_Jose_AP1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Michael_Jose_AP1_P1.DAL
 {
-    public class Contexto
+    public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
     {
+        public DbSet<Registro> Registros { get; set; }
     }
 }
