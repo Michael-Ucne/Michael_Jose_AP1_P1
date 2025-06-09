e9d0b6516ad8b1a35b28b1c9ab91d4a5a83746ee
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-09 18:53:01 -0400

    Agrega el metodo inserta

--- Services/RegistroServices.cs
+++ Services/RegistroServices.cs
@@ -1,10 +1,17 @@
 ﻿using Michael_Jose_AP1_P1.DAL;
+using Michael_Jose_AP1_P1.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Michael_Jose_AP1_P1.Services
 {
     public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
     {
+        private async Task<bool> Insertar(Registro aporte)
+        {
+            await using var contexto = await DbFactory.CreateDbContextAsync();
+            contexto.Registros.Add(aporte);
+            return await contexto.SaveChangesAsync() > 0;
+        }
 
     }
 }
