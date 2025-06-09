035ea72f1e1a2cef1b0e9f5119528a82898aaaf4
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-09 19:14:50 -0400

    Mi Primera Migracion

---
+++ Migrations/20250609231439_Primera migracion.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Michael_Jose_AP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Primeramigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Registros",
+                columns: table => new
+                {
+                    AporteId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Mount = table.Column<double>(type: "double precision", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registros", x => x.AporteId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Registros");
+        }
+    }
+}
