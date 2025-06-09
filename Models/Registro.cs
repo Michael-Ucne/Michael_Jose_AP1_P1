e7814fbb3eb7f1da609a8cef6ff6a7713486ab0d
Michael Jose Vasquez <mjv1990fake@gmail.com>
2025-06-09 18:33:54 -0400

    Agrega los parametros del Modelo de Registro

--- Models/Registro.cs
+++ Models/Registro.cs
@@ -1,6 +1,15 @@
-﻿namespace Michael_Jose_AP1_P1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Michael_Jose_AP1_P1.Models
 {
     public class Registro
     {
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "El Nombre es requerido")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "El Monto de Aporte es requerido")]
+        public double Mount{ get; set; }
     }
 }
